3ccffc52f12a8b3da78ecf86a8e3703e098dd9cb
Icarus-21974 <adamyaris71@gmail.com>
2025-04-08 11:43:50 -0400

    Update StartingScreen.cs

--- StartingScreen.cs
+++ StartingScreen.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class StartingScreen : MonoBehaviour
 {
+
+    public AudioSource mainMenuAudio;
     // Start is called before the first frame update
     void Start()
     {
