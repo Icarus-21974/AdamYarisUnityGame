ef8236fc9d66ca82ba92bdef2c76c99b2d540fa6
Icarus-21974 <adamyaris71@gmail.com>
2025-04-08 08:40:00 -0400

    Create StartingScreen.cs

---
+++ StartingScreen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartingScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            changeScene("AdamYarisCreate3");
+        }
+    }
+
+    public void changeScene(string scenename)
+    {
+        SceneManager.GetSceneByName(scenename);
+        SceneManager.LoadScene(scenename);
+    }
+}
